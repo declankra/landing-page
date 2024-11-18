22e4e2d60a4de1c9fe3e44512a624944377c4dd1
declankra <declankramper@gmail.com>
2024-11-18 13:27:03 -0600

    added dark mode support, determined by user preference

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,22 @@
 import '../styles/globals.css';
 import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import { ThemeProvider } from 'next-themes';
 import { Toaster } from '@/components/ui/toaster'; // Import from shadcn
 import { theme as mantineTheme } from '../theme/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // Using Mantine's provider on the outside to allow shadcn components to override when needed
-    <MantineProvider theme={mantineTheme} defaultColorScheme="light">
-      <Component {...pageProps} />
-      <Toaster /> {/* shadcn Toaster */}
-    </MantineProvider>
+    <>
+      <ColorSchemeScript />
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <MantineProvider theme={mantineTheme}>
+          <Component {...pageProps} />
+          <Toaster /> {/* shadcn Toaster */}
+        </MantineProvider>
+      </ThemeProvider>
+    </>
   );
 }
