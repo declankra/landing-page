4c2f5504972012a064e65545ffa8b61d1857fb90
declankra <declankramper@gmail.com>
2024-11-24 14:20:58 -0600

    ignore supabase functions directory during build, it gets deployed seperately

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      ...(config.watchOptions || {}),
+      ignored: [...(config.watchOptions?.ignored || []), '**/supabase/functions/**'],
+    };
+    return config;
+  },
+  
 };
 
 export default nextConfig;
